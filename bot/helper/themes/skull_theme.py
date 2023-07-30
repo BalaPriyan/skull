b4caf578db93d65fa1a4294237876236ee962346
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-07-30 05:39:03 -0700

    Create skull_theme.py

---
+++ bot/helper/themes/skull_theme.py
@@ -0,0 +1,10 @@
+class SKULL:
+  
+    ST_BN1_NAME = 'Repo'
+    ST_BN1_URL = 'https://github.com/BalaPriyan/skull'
+    ST_BN2_NAME = 'Updates'
+    ST_BN2_URL = 'https://t.me/WZML_X'
+    ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
+<b>Type {help_command} to get a list of available commands</b>'''
+    ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
+    ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</i>'''
